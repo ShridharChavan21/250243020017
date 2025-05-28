fa1d3f5a9e4e75e426ba05477da13f0793c92e8b
ShridharChavan21 <153070215+ShridharChavan21@users.noreply.github.com>
2025-05-28 09:56:23 +0530

    First commit

---
+++ api/DateDemo.java
@@ -0,0 +1,13 @@
+package apps;
+
+import office.utility.Date;
+
+public class DateDemo {
+
+	public static void main(String[] args) {
+		Date d1 = new Date(12,11,2000);
+		d1.display('=');
+
+	}
+
+}
